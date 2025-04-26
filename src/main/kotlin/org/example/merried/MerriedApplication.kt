ff243b1756ac7abecfa034484d200cce7bd6265d
anys34 <anyeseong34@gmail.com>
2025-04-26 16:57:04 +0900

    init

---
+++ src/main/kotlin/org/example/merried/MerriedApplication.kt
@@ -0,0 +1,11 @@
+package org.example.merried
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MerriedApplication
+
+fun main(args: Array<String>) {
+    runApplication<MerriedApplication>(*args)
+}
