de5e162c1d8b87f2c2cdaf6858ac5fc4468d4f17
anys34 <anyeseong34@gmail.com>
2025-04-29 02:59:20 +0900

    feat:: auth details

---
+++ src/main/kotlin/org/example/merried/global/security/auth/AuthDetails.kt
@@ -0,0 +1,22 @@
+package org.example.merried.global.security.auth
+
+import org.example.merried.domain.user.domain.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+    private val user: User,
+): UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return mutableListOf(SimpleGrantedAuthority("ROLE_USER"))
+    }
+
+    override fun getPassword(): String? {
+        return null
+    }
+
+    override fun getUsername(): String {
+        return user.email
+    }
+}
