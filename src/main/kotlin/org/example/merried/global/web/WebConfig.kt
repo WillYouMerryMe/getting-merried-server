f9ba5fab21250958163cabd46b3c42b46338cd92
anys34 <anyeseong34@gmail.com>
2025-04-29 03:06:08 +0900

    feat:: web config

---
+++ src/main/kotlin/org/example/merried/global/web/WebConfig.kt
@@ -0,0 +1,29 @@
+package org.example.merried.global.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.OPTIONS.name(),
+            )
+            .allowedOrigins(
+                "http://localhost:3000"
+            )
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
