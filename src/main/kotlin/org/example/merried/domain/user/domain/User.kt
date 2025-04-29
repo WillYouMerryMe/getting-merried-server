66a2e75a24b335b6facbfb382d20152ae34c560a
anys34 <anyeseong34@gmail.com>
2025-04-29 10:08:55 +0900

    add:: user

--- src/main/kotlin/org/example/merried/domain/user/domain/User.kt
+++ src/main/kotlin/org/example/merried/domain/user/domain/User.kt
@@ -2,12 +2,21 @@ package org.example.merried.domain.user.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import org.example.merried.domain.user.domain.type.Provider
 import org.example.merried.global.entity.BaseEntity
 
 @Entity
 data class User(
     @Column(unique = true)
     val email: String,
-    val username: String,
+    val nickname: String,
+
+    @Column(name = "profile_img")
+    val profileImg: String,
+
+    @Enumerated(EnumType.STRING)
+    val provider: Provider,
 ): BaseEntity() {
 }
---
+++ src/main/kotlin/org/example/merried/domain/user/domain/type/Provider.kt
@@ -0,0 +1,6 @@
+package org.example.merried.domain.user.domain.type
+
+enum class Provider {
+    NAVER,
+    KAKAO,
+}
