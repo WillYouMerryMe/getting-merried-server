4f3da236c9603dd1a05cbcb157c2762a97c44824
anys34 <anyeseong34@gmail.com>
2025-04-29 09:55:29 +0900

    feat:: kakao auth

---
+++ src/main/kotlin/org/example/merried/global/feign/auth/kakao/KakaoAuthClient.kt
@@ -0,0 +1,16 @@
+package org.example.merried.global.feign.auth.kakao
+
+import org.example.merried.global.feign.auth.kakao.response.KakaoAuthResponse
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@FeignClient(name = "KakaoAuthClient", url = "https://kauth.kakao.com")
+interface KakaoAuthClient {
+    @GetMapping("/oauth/token?grant_type=authorization_code")
+    fun getAccessToken(
+        @RequestParam("client_id") clientId: String,
+        @RequestParam("redirect_uri") uri: String,
+        @RequestParam("code") code: String,
+    ): KakaoAuthResponse
+}
---
+++ src/main/kotlin/org/example/merried/global/feign/auth/kakao/response/KakaoUserProfileResponse.kt
@@ -0,0 +1,15 @@
+package org.example.merried.global.feign.auth.kakao.response
+
+data class KakaoUserProfileResponse(
+    val kakao_account: KakaoAccount,
+) {
+    data class KakaoAccount(
+        val profile: KakaoProfile,
+    )
+
+    data class KakaoProfile(
+        val email: String,
+        val nickname: String,
+        val profile_image_url: String,
+    )
+}
