4e6231a31ae087975cf8817042a69e40f39c9b17
anys34 <anyeseong34@gmail.com>
2025-07-04 11:10:00 +0900

    fix: 포토 갤러리 형식 수정

--- src/main/kotlin/org/example/married/domain/card/domain/component/PhotoGallery.kt
+++ src/main/kotlin/org/example/married/domain/card/domain/component/PhotoGallery.kt
@@ -1,34 +1,18 @@
 package org.example.married.domain.card.domain.component
 
-import kotlin.String
-
 data class PhotoGallery(
-    val galleries: List<PhotoGalleryDetail>,
-)
-
-data class PhotoGalleryDetail(
     val title: String,
-    val url: String,
+    val urls: List<String>,
 )
 
 data class PhotoGalleryDto(
-    val galleries: List<PhotoGalleryDetail>,
+    val title: String,
+    val urls: List<String>,
 ) {
     fun toPhotoGallery(): PhotoGallery {
         return PhotoGallery(
-            galleries = galleries
-        )
-    }
-}
-
-data class PhotoGalleryDetailDto(
-    val title: String,
-    val url: String,
-) {
-    fun toPhotoGallery(): PhotoGalleryDetail {
-        return PhotoGalleryDetail(
             title = title,
-            url = url,
+            urls = urls,
         )
     }
 }
